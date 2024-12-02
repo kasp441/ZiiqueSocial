75df39f30f21179c53e36eca41f775514eb31063
kasp441 <91524039+kasp441@users.noreply.github.com>
2024-12-02 11:18:47 +0100

    Update backend/Domain/Login.cs
    Co-authored-by: Casper Fink <47432671+zeroject@users.noreply.github.com>

--- backend/Domain/Login.cs
+++ backend/Domain/Login.cs
@@ -3,7 +3,7 @@
     public class Login
     {
         public int Id { get; set; }
-        public string? email { get; set; }
+        public string email { get; set; }
         public required string username { get; set; }
         public required byte[] password { get; set; }
     }
