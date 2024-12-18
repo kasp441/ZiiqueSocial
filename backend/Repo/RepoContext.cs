156f71fe4540081d3c0428a960eaf3df21b696d2
Casper Fink <47432671+zeroject@users.noreply.github.com>
2024-12-18 13:28:24 +0100

    fixes

--- backend/Repo/RepoContext.cs
+++ backend/Repo/RepoContext.cs
@@ -1,13 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Domain;
 
-
 namespace Repo
 {
     public class RepoContext : DbContext
     {
         public DbSet<Profile> Profiles { get; set; }
-        public DbSet<Login> Logins { get; set; }
         public DbSet<Post> Posts { get; set; }
         public DbSet<Follows> Follows { get; set; }
         public RepoContext(DbContextOptions<RepoContext> options) : base(options)
@@ -22,6 +20,8 @@ namespace Repo
 
             modelBuilder.Entity<Post>()
                 .HasKey(p => p.Id);
+            modelBuilder.Entity<Post>()
+                .HasOne<Profile>().WithMany();
         }
     }
 }
---
+++ backend/ZiiqueSocialBackend/Migrations/20241218122758_post_now_has_acation_with_profile.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace ZiiqueSocialBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class post_now_has_acation_with_profile : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Logins");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Posts_ProfileId",
+                table: "Posts",
+                column: "ProfileId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Posts_Profiles_ProfileId",
+                table: "Posts",
+                column: "ProfileId",
+                principalTable: "Profiles",
+                principalColumn: "Guid",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Posts_Profiles_ProfileId",
+                table: "Posts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Posts_ProfileId",
+                table: "Posts");
+
+            migrationBuilder.CreateTable(
+                name: "Logins",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    email = table.Column<string>(type: "text", nullable: false),
+                    password = table.Column<byte[]>(type: "bytea", nullable: false),
+                    username = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Logins", x => x.Id);
+                });
+        }
+    }
+}
